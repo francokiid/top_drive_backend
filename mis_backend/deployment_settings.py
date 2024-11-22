92456bb8525741fa05bb55bed1155ba6ff9abcc4
Top Drive <v.angelfranco@gmail.com>
2024-11-23 06:46:10 +0800

    Updated deployment settings

--- mis_backend/deployment_settings.py
+++ mis_backend/deployment_settings.py
@@ -21,9 +21,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://top-drive-frontend.onrender.com',
+]
 
 STORAGES = {
     'default': {
